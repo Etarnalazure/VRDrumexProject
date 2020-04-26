fff6b2fbaf01d60fda7bed3c4329e089d93908ee
MichaelBlaxkjaer <56006302+MichaelBlaxkjaer@users.noreply.github.com>
2020-04-26 11:36:04 +0200

    Video intro added

---
+++ Script/VideoFadeOut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VideoFadeOut : MonoBehaviour
+{
+
+    public RawImage videoFade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoFade.canvasRenderer.SetAlpha(20.0f);
+
+        fadeOut();
+    }
+
+    // Update is called once per frame
+    void fadeOut()
+    {
+        videoFade.CrossFadeAlpha(0, 7, false);
+    }
+}

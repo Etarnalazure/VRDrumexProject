09c320bf594ce9594fb7e01ccc97366e4d5b4873
MichaelBlaxkjaer <michaelblaxkjaer@gmail.com>
2020-05-14 15:48:57 +0200

    Scene Changer added
    Added a script with a time changer and placed it on scene01. Also saved our scenes as "Scene01" and "Scene02"

---
+++ Assets/Script/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public int sceneTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ChangeScene", sceneTime);
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Scene02");
+    }
+    
+}
